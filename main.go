34bcfe7a48e03d47f222eca6729e3871e3ad39b8
Marco Piovan <marco.piovan@gmail.com>
2017-01-03 22:29:40 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	apiKey := os.Getenv("TELEGRAM_API_KEY")
+	bot, err := tgbotapi.NewBotAPI(apiKey)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
